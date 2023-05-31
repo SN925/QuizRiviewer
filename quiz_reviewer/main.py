bbca9b44054a318923214eece0dbafaf064a8a7a
SN925 <pkmntaiko925630@gmail.com>
2023-06-01 01:02:35 +0900

    finish

--- quiz_reviewer/main.py
+++ quiz_reviewer/main.py
@@ -2,17 +2,16 @@ from flask import Flask
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-#app = Flask(__name__)  #アプリケーションのインスタンス化
-app = Flask(__name__, static_folder='./static')
+app = Flask(__name__)  #アプリケーションのインスタンス化
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///quiz.db"  #データベース作成のパス
-db = SQLAlchemy(app) #これでデータベース生成
+db = SQLAlchemy(app) #データベース生成
 
 
 class Quizset(db.Model):
     quizsetname = db.Column(db.String)
-    id = db.Column(db.Integer, primary_key=True)  #勝手に追加される値
-    quiz_sentence = db.Column(db.String(100),nullable=False) #()内は文字制限、nullableは空文字防ぎ
-    answer = db.Column(db.String(20), nullable=False) #uniqueをオンにすると同じ名前をダメにできる
+    id = db.Column(db.Integer, primary_key=True)  
+    quiz_sentence = db.Column(db.String(100),nullable=False) 
+    answer = db.Column(db.String(20), nullable=False) 
     reading = db.Column(db.String(20), nullable=False)
 
 class Quizsettype(db.Model):
@@ -30,9 +29,9 @@ def top():
 @app.route("/managetop", methods=["GET","POST"]) 
 def managetop():
     if request.method == "GET":
-        quiz_tuple = Quizset.query.with_entities(Quizsettype.name).all()  #nameカラム全取得
-        quiz_list = [n[0] for n in quiz_tuple]   #クエリをリスト化する
-        quizsetname = sorted(set(quiz_list))   #重複しない要素のみにする
+        quiz_tuple = Quizset.query.with_entities(Quizsettype.name).all()  
+        quiz_list = [n[0] for n in quiz_tuple]  
+        quizsetname = sorted(set(quiz_list)) 
         return render_template("html/managetop.html", quizsetname=quizsetname)
 
 
@@ -52,14 +51,14 @@ def manage_quiz(id):
         return render_template("html/manage_quiz.html",quizes=quizes)
     else:
         quiz = Quizset.query.filter_by(id=id)
-        post = Quizset.query.get(id)  #指定したidの投稿のみを取ってくる
-        post.quiz_sentence = request.form.get("quiz_sentence") #インスタンス化してるのでこれで良いと。
+        post = Quizset.query.get(id) 
+        post.quiz_sentence = request.form.get("quiz_sentence") 
         post.answer = request.form.get("answer")
         post.reading = request.form.get("reading")
-        quizsetname = post.quizsetname  #ここでquizsetnameの中身を取り出したいので聞く。
+        quizsetname = post.quizsetname 
         quizes = Quizset.query.filter_by(quizsetname = quizsetname)
         if post.quiz_sentence != "" and post.answer != "" and post.reading != "":
-            db.session.commit()  #上書き＆変更を保存（addは更新の場合はいらない）
+            db.session.commit() 
             return render_template("html/manage_quizset.html",quizes=quizes,quizsetname=quizsetname)
         else:
             error = "入力していない項目があります"
@@ -97,7 +96,7 @@ def quizsetcreate():
     if request.method == "POST":
         name = request.form.get("name")
         if name != "":
-            post = Quizsettype(name=name) #Postクラスのtitleに、取得したtitleを入れる。bodyも。
+            post = Quizsettype(name=name) 
             db.session.add(post)  
             db.session.commit()  
 
@@ -115,20 +114,20 @@ def quizsetcreate():
 #新規クイズの作成。
 @app.route("/<string:quizsetname>/quizcreate", methods=["GET","POST"])  
 def quizcreate(quizsetname):
-    if request.method == "POST": #POSTはデータが送信された時
-        quiz_sentence = request.form.get("quiz_sentence") #htmlから取得する。
+    if request.method == "POST": 
+        quiz_sentence = request.form.get("quiz_sentence") 
         answer = request.form.get("answer")
         reading = request.form.get("reading")
 
         if quiz_sentence != "" and answer != "" and reading != "":
             quizsetname = quizsetname
-            post = Quizset(quiz_sentence=quiz_sentence, answer=answer, reading=reading, quizsetname=quizsetname) #Postクラスのtitleに、取得したtitleを入れる。bodyも。
+            post = Quizset(quiz_sentence=quiz_sentence, answer=answer, reading=reading, quizsetname=quizsetname)
 
-            db.session.add(post)  #これで追加
-            db.session.commit()  #変更を保存
+            db.session.add(post) 
+            db.session.commit()  
 
             quizes = Quizset.query.filter_by(quizsetname = quizsetname)
-            return render_template("html/manage_quizset.html",quizes=quizes,quizsetname=quizsetname)   #自動で移動してくれる
+            return render_template("html/manage_quizset.html",quizes=quizes,quizsetname=quizsetname) 
         else:
             error = "入力していない項目があります"
             return render_template("html/quizcreate.html",error=error,quizsetname=quizsetname) 
@@ -141,15 +140,15 @@ def quizcreate(quizsetname):
 def solvetop():
         global correct,order
         correct,order = 0,-1
-        quiz_tuple = Quizset.query.with_entities(Quizset.quizsetname).all() #("")消す方法不明
+        quiz_tuple = Quizset.query.with_entities(Quizset.quizsetname).all()
         quiz_list = [n[0] for n in quiz_tuple]
         quizsetname = sorted(set(quiz_list))
-        return render_template("html/solvetop.html", quizsetname=quizsetname) #変数をいくつも渡すことも可能
+        return render_template("html/solvetop.html", quizsetname=quizsetname)
 
 
 #クイズを解いている時の画面。問題の表示と正解数の記録、正誤の判定等を行う
 order,correct,end = -1,0,False   #何問目か,正解数,全問題を解いたか否か
-@app.route("/<string:quizsetname>/solving", methods=["GET","POST"])  #ルーティング。これ" "の部分を変数にしていじれそう。
+@app.route("/<string:quizsetname>/solving", methods=["GET","POST"])  
 def solving(quizsetname):
     global order,correct,end
     if request.method == "GET":
@@ -176,25 +175,25 @@ def solving(quizsetname):
         
 
 #クイズの結果の表示
-@app.route("/result", methods=["GET","POST"])  #ルーティング。これ" "の部分を変数にしていじれそう。
+@app.route("/result", methods=["GET","POST"]) 
 def result():
     global correct
     global order
     global end
     if request.method == "GET":
-        quiz_tuple = Quizset.query.with_entities(Quizset.quizsetname).all() #("")消す方法不明
+        quiz_tuple = Quizset.query.with_entities(Quizset.quizsetname).all()
         quiz_list = [n[0] for n in quiz_tuple]
         quizsetname = sorted(set(quiz_list))
         correct,order,end = 0,-1,False
-        return render_template("html/solvetop.html", quizsetname=quizsetname) #変数をいくつも渡すことも可能
+        return render_template("html/solvetop.html", quizsetname=quizsetname)
     else:
-        quiz_tuple = Quizset.query.with_entities(Quizset.quizsetname).all() #("")消す方法不明
+        quiz_tuple = Quizset.query.with_entities(Quizset.quizsetname).all()
         quiz_list = [n[0] for n in quiz_tuple]
         quizsetname = sorted(set(quiz_list))
 
         quizsetname = request.form.get("quizsetname")
 
-        quizes = Quizset.query.filter_by(quizsetname = quizsetname) #これではよく分からん英語
+        quizes = Quizset.query.filter_by(quizsetname = quizsetname) #クエリが表示
         quiz_sum = []
         for i in quizes:   
             quiz_sum.append(i.answer)
